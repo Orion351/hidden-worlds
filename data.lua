973974497f946f65716091ad0c5304a267e57f17
Orion351 <derrickst@gmail.com>
2025-03-14 07:04:28 -0400

    primitive icons progress

---
+++ data.lua
@@ -0,0 +1,241 @@
+---@ class HW_globals
+HW_globals = HW_globals or {}
+
+
+
+data:extend{
+  {
+    type = "item",
+    name = "rope",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "timber-hardwood",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "bark",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/woods/bark/bark-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "dry-clay",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "wet-clay",
+    stack_size = 100,
+    spoil_ticks = 216000,
+    spoil_result = "dry-clay",
+    icon = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "timber-softwood",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "timber-stripped-softwood",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+  {
+    type = "item",
+    name = "timber-stripped-hardwood",
+    stack_size = 100,
+    icon = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0000.png",
+    icon_size = 128,
+    pictures = {
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0000.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0001.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0002.png",
+        size = 128,
+        scale = 0.25,
+      },
+      {
+        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0003.png",
+        size = 128,
+        scale = 0.25,
+      },
+    }
+  },
+}
