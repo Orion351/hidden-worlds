55b5689e0dc300ac31c20cac60b8a2865109e757
Orion351 <derrickst@gmail.com>
2025-03-19 15:33:23 -0400

    asdf

--- data.lua
+++ data.lua
@@ -26,3 +26,35 @@ for dir, things in pairs(items) do
 end
 
 -- Compound Entity POC
+
+data:extend{
+  {
+    type = "item",
+    name = "dispatcher",
+    stack_size = 5,
+    icon = data.raw.item["assembling-machine-1"].icon,
+    icon_size = data.raw.item["assembling-machine-1"].icon_size,
+    place_result = "dispatch-cargo",
+  },
+  {
+    type = "container",
+    name = "dispatch-cargo",
+    inventory_size = 100,
+    max_health = 350,
+    circuit_connector = data.raw["assembling-machine"]["assembling-machine-1"].circuit_connector,
+    circuit_wire_max_distance = 9,
+    inventory_type = "with_filters_and_bar",
+    picture = {
+      filename = "__hidden-worlds__/graphics/entity/dispatcher/dispatcher.png",
+      size = {214, 226},
+      scale = 0.5,
+    },
+    minable = {
+      results = {{type = "item", name = "dispatcher", amount = 1}},
+      mining_time = .5,
+    },
+    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  }
+}
+
