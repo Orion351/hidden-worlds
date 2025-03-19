7c379618d3ea98703605236e9bad167af5871b69
Orion351 <derrickst@gmail.com>
2025-03-19 13:13:11 -0400

    asdf

--- data.lua
+++ data.lua
@@ -1,241 +1,28 @@
 ---@ class HW_globals
 HW_globals = HW_globals or {}
 
-
-
-data:extend{
-  {
-    type = "item",
-    name = "rope",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/primitive-intermeidates/rope/rope-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "timber-hardwood",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-hardwood/timber-hardwood-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "bark",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/woods/bark/bark-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/bark/bark-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "dry-clay",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/dry-clay/dry-clay-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "wet-clay",
-    stack_size = 100,
-    spoil_ticks = 216000,
-    spoil_result = "dry-clay",
-    icon = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/earths/wet-clay/wet-clay-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "timber-softwood",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-softwood/timber-softwood-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "timber-stripped-softwood",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-softwood/timber-stripped-softwood-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
-  {
-    type = "item",
-    name = "timber-stripped-hardwood",
-    stack_size = 100,
-    icon = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0000.png",
-    icon_size = 128,
-    pictures = {
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0000.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0001.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0002.png",
-        size = 128,
-        scale = 0.25,
-      },
-      {
-        filename = "__hidden-worlds__/graphics/icons/woods/timber-stripped-hardwood/timber-stripped-hardwood-0003.png",
-        size = 128,
-        scale = 0.25,
-      },
-    }
-  },
+local items = {
+  ["primitive-intermeidates"] = {"rope"},
+  ["earths"] = {"dry-clay", "wet-clay"},
+  ["woods"] = {"bark", "timber-hardwood", "timber-softwood", "lumber-hardwood", "lumber-softwood", "timber-split-hardwood", "timber-split-softwood", "lumber-split-hardwood", "lumber-split-softwood"}
 }
+
+for dir, things in pairs(items) do
+  for _, thing in pairs(things) do
+    local pics = {}
+    for i = 0, 1, 1 do
+      table.insert(pics, {size = 128, scale = 0.25, filename = "__hidden-worlds__/graphics/icons/" .. dir .. "/" .. thing .. "/" .. thing .. "-000" .. i .. ".png"})
+    end
+    
+    data:extend{{
+      type = "item",
+      name = thing,
+      stack_size = 100,
+      icon_size = 128,
+      icon = "__hidden-worlds__/graphics/icons/" .. dir .. "/" .. thing .. "/" .. thing .. "-0000.png",
+      pictures = pics
+    }}
+  end
+end
+
+-- Compound Entity POC
