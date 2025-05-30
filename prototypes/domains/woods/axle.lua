d537e8b5fd2e9373c2961fa598cc7289209730be
Jurgen <jurgen.baas@outlook.com>
2025-05-30 18:00:11 +0200

    Changed indentation to 2 spaces to match rest of project

--- prototypes/domains/woods/axle.lua
+++ prototypes/domains/woods/axle.lua
@@ -1,122 +1,122 @@
 ---@type data.RecipeCategory
 local axleRecipeCategory = {
-    name = "axle",
-    type = "recipe-category"
+  name = "axle",
+  type = "recipe-category"
 }
 
 ---@type data.RecipePrototype
 local axleTorqueRecipe = {
-    name = "axle-torque-recipe",
-    type = "recipe",
-    category = axleRecipeCategory.name,
-    ingredients = {
-        {type = "fluid", name="torque", amount=100}
-    },
-    results = {
-        {type = "fluid", name = "torque", amount = 90}
-    }
+  name = "axle-torque-recipe",
+  type = "recipe",
+  category = axleRecipeCategory.name,
+  ingredients = {
+    {type = "fluid", name="torque", amount=100}
+  },
+  results = {
+    {type = "fluid", name = "torque", amount = 90}
+  }
 }
 
 ---@type data.AssemblingMachinePrototype
 local axleEntity = {
-    name = "axle",
-    type = "assembling-machine",
-    fixed_recipe = axleTorqueRecipe.name,
-    crafting_speed = 1,
-    crafting_categories = {"axle"},
-    energy_usage = "1W",
-    energy_source = {type = "void"},
-    minable = {mining_time = 0.2, result = "axle"},
-
-    collision_box = {{-0.4, -2.4}, {0.4, 2.4}},
-    selection_box = {{-0.5, -2.5}, {0.5, 2.5}},
-    fluid_boxes = {
+  name = "axle",
+  type = "assembling-machine",
+  fixed_recipe = axleTorqueRecipe.name,
+  crafting_speed = 1,
+  crafting_categories = {"axle"},
+  energy_usage = "1W",
+  energy_source = {type = "void"},
+  minable = {mining_time = 0.2, result = "axle"},
+  
+  collision_box = {{-0.4, -2.4}, {0.4, 2.4}},
+  selection_box = {{-0.5, -2.5}, {0.5, 2.5}},
+  fluid_boxes = {
+    {
+      volume = 100,
+      filter = "torque",
+      production_type = "input",
+      pipe_connections = {
         {
-            volume = 100,
-            filter = "torque",
-            production_type = "input",
-            pipe_connections = {
-                {
-                    flow_direction = "input-output",
-                    direction = defines.direction.north --[[@as data.Direction]],
-                    position = {0, -2}
-                }, {
-                    flow_direction = "input-output",
-                    direction = defines.direction.south --[[@as data.Direction]],
-                    position = {0, 2}
-                }
-            }
-
+          flow_direction = "input-output",
+          direction = defines.direction.north --[[@as data.Direction]],
+          position = {0, -2}
         }, {
-            volume = 100,
-            filter = "torque",
-            production_type = "output",
-            pipe_connections = {
-                {
-                    flow_direction = "output",
-                    direction = defines.direction.west --[[@as data.Direction]],
-                    position = {0, -2}
-                }, {
-                    flow_direction = "output",
-                    direction = defines.direction.east --[[@as data.Direction]],
-                    position = {0, -2}
-                }, {
-                    flow_direction = "output",
-                    direction = defines.direction.west --[[@as data.Direction]],
-                    position = {0, 2}
-                }, {
-                    flow_direction = "output",
-                    direction = defines.direction.east --[[@as data.Direction]],
-                    position = {0, 2}
-                }
-            }
+          flow_direction = "input-output",
+          direction = defines.direction.south --[[@as data.Direction]],
+          position = {0, 2}
         }
-    },
-    icon = "__hidden-worlds__/graphics/icons/entity/axle.png",
-    graphics_set = {
-        animation = {
-            north = {
-                filename = "__hidden-worlds__/graphics/greybox/grey-boxes/hw-grey-box-1x5.png",
-                width = 64,
-                height = 320,
-                scale = 0.5
-            },
-            west = {
-                filename = "__hidden-worlds__/graphics/greybox/grey-boxes/hw-grey-box-5x1.png",
-                width = 320,
-                height = 64,
-                scale = 0.5
-            }
+      }
+      
+    }, {
+      volume = 100,
+      filter = "torque",
+      production_type = "output",
+      pipe_connections = {
+        {
+          flow_direction = "output",
+          direction = defines.direction.west --[[@as data.Direction]],
+          position = {0, -2}
+        }, {
+          flow_direction = "output",
+          direction = defines.direction.east --[[@as data.Direction]],
+          position = {0, -2}
+        }, {
+          flow_direction = "output",
+          direction = defines.direction.west --[[@as data.Direction]],
+          position = {0, 2}
+        }, {
+          flow_direction = "output",
+          direction = defines.direction.east --[[@as data.Direction]],
+          position = {0, 2}
         }
+      }
     }
+  },
+  icon = "__hidden-worlds__/graphics/icons/entity/axle.png",
+  graphics_set = {
+    animation = {
+      north = {
+        filename = "__hidden-worlds__/graphics/greybox/grey-boxes/hw-grey-box-1x5.png",
+        width = 64,
+        height = 320,
+        scale = 0.5
+      },
+      west = {
+        filename = "__hidden-worlds__/graphics/greybox/grey-boxes/hw-grey-box-5x1.png",
+        width = 320,
+        height = 64,
+        scale = 0.5
+      }
+    }
+  }
 }
 axleEntity.graphics_set.animation.south = axleEntity.graphics_set.animation.north
 axleEntity.graphics_set.animation.east = axleEntity.graphics_set.animation.west
 
 ---@type data.ItemPrototype
 local axleItem = {
-    type = "item",
-    name = axleEntity.name,
-    icon = axleEntity.icon,
-    icon_size = 64,
-    flags = {},
-    subgroup = "extraction-machine",
-    --order = "za"..name,
-    place_result = axleEntity.name,
-    stack_size = 50
+  type = "item",
+  name = axleEntity.name,
+  icon = axleEntity.icon,
+  icon_size = 64,
+  flags = {},
+  subgroup = "extraction-machine",
+  --order = "za"..name,
+  place_result = axleEntity.name,
+  stack_size = 50
 }
 
 ---@type data.RecipePrototype
 local axleRecipe = {
-    name = "axle-recipe",
-    type = "recipe",
-    category = "basic-crafting",
-    ingredients = {
-        -- TODO
-    },
-    results = {
-        {type = "item", name = axleItem.name, amount = 1}
-    }
+  name = "axle-recipe",
+  type = "recipe",
+  category = "basic-crafting",
+  ingredients = {
+    -- TODO
+  },
+  results = {
+    {type = "item", name = axleItem.name, amount = 1}
+  }
 }
 
 ---@diagnostic disable-next-line: assign-type-mismatch
--- prototypes/domains/woods/water-mill.lua
+++ prototypes/domains/woods/water-mill.lua
@@ -1,121 +1,121 @@
 ---@type data.InfinityPipePrototype
 local linked_water_pipe = {
-    name = "linked_water_pipe",
-    type = "infinity-pipe",
-    gui_mode = "none",
-    icon = "__base__/graphics/icons/fluid/water.png",
-    fluid_box = {
-        volume = 100,
-        hide_connection_info = true,
-        pipe_connections = {{
-            --flow_direction = "output",
-            connection_type = "linked",
-            linked_connection_id  = 820
-        }},
-        filter = "water",
-        production_type = "output",
-    },
-    horizontal_window_bounding_box = {{0, 0}, {0, 0}},
-    vertical_window_bounding_box = {{0, 0}, {0, 0}},
-    collision_mask = {layers = {}},
-    icon_draw_specification = {scale = 0.0},
-    hidden = true
+  name = "linked_water_pipe",
+  type = "infinity-pipe",
+  gui_mode = "none",
+  icon = "__base__/graphics/icons/fluid/water.png",
+  fluid_box = {
+    volume = 100,
+    hide_connection_info = true,
+    pipe_connections = {{
+      --flow_direction = "output",
+      connection_type = "linked",
+      linked_connection_id  = 820
+    }},
+    filter = "water",
+    production_type = "output",
+  },
+  horizontal_window_bounding_box = {{0, 0}, {0, 0}},
+  vertical_window_bounding_box = {{0, 0}, {0, 0}},
+  collision_mask = {layers = {}},
+  icon_draw_specification = {scale = 0.0},
+  hidden = true
 }
 
 ---@type data.RecipeCategory
 local waterMillRecipeCategory = {
-    name = "water-mill",
-    type = "recipe-category"
+  name = "water-mill",
+  type = "recipe-category"
 }
 
 ---@type data.RecipePrototype
 local waterMillTorqueRecipe = {
-    name = "water-mill-torque-recipe",
-    type = "recipe",
-    category = waterMillRecipeCategory.name,
-    ingredients = {
-        {type="fluid", name="water", amount=50}
-    },
-    results = {
-        {type = "fluid", name = "torque", amount = 100}
-    }
+  name = "water-mill-torque-recipe",
+  type = "recipe",
+  category = waterMillRecipeCategory.name,
+  ingredients = {
+    {type="fluid", name="water", amount=50}
+  },
+  results = {
+    {type = "fluid", name = "torque", amount = 100}
+  }
 }
 
 ---@type data.AssemblingMachinePrototype
 local waterMill = {
-    name = "water-mill",
-    type = "assembling-machine",
-    fixed_recipe = waterMillTorqueRecipe.name,
-    crafting_speed = 1,
-    crafting_categories = {"water-mill"},
-    energy_usage = "1W",
-    energy_source = {type = "void"},
-    minable = {mining_time = 0.2, result = "water-mill"},
-
-    collision_mask = {layers={object=true, train=true, is_object=true, is_lower_object=true, meltable=true}},
-    collision_box = {{-1.4, -1.3}, {1.4, 1.4}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    fluid_boxes = {
-        {
-            volume = 100,
-            filter = "water",
-            production_type = "input",
-            pipe_connections = {{
-                connection_type = "linked",
-                linked_connection_id  = 820
-            }}
-        },
-        {
-            volume = 100,
-            filter = "torque",
-            production_type = "output",
-            pipe_connections = {{
-                flow_direction = "output",
-                direction = defines.direction.north --[[@as data.Direction]],
-                position = {0, -1}
-            }}
-        }
+  name = "water-mill",
+  type = "assembling-machine",
+  fixed_recipe = waterMillTorqueRecipe.name,
+  crafting_speed = 1,
+  crafting_categories = {"water-mill"},
+  energy_usage = "1W",
+  energy_source = {type = "void"},
+  minable = {mining_time = 0.2, result = "water-mill"},
+  
+  collision_mask = {layers={object=true, train=true, is_object=true, is_lower_object=true, meltable=true}},
+  collision_box = {{-1.4, -1.3}, {1.4, 1.4}},
+  selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  fluid_boxes = {
+    {
+      volume = 100,
+      filter = "water",
+      production_type = "input",
+      pipe_connections = {{
+        connection_type = "linked",
+        linked_connection_id  = 820
+      }}
     },
-    tile_buildability_rules = {
-        {
-            area = {{-1.2, -1.2}, {1.2, -0.7}},
-            required_tiles = {layers={ground_tile=true}},
-            colliding_tiles = {layers={water_tile=true}},
-            remove_on_collision = false
-        },
-        {
-            area = {{-1.2, 0}, {1, 1.2}},
-            required_tiles = {layers={water_tile=true}},
-            colliding_tiles = {layers={}}
-        },
+    {
+      volume = 100,
+      filter = "torque",
+      production_type = "output",
+      pipe_connections = {{
+        flow_direction = "output",
+        direction = defines.direction.north --[[@as data.Direction]],
+        position = {0, -1}
+      }}
+    }
+  },
+  tile_buildability_rules = {
+    {
+      area = {{-1.2, -1.2}, {1.2, -0.7}},
+      required_tiles = {layers={ground_tile=true}},
+      colliding_tiles = {layers={water_tile=true}},
+      remove_on_collision = false
     },
-    icon = "__hidden-worlds__/graphics/icons/entity/waterMill.png"
+    {
+      area = {{-1.2, 0}, {1, 1.2}},
+      required_tiles = {layers={water_tile=true}},
+      colliding_tiles = {layers={}}
+    },
+  },
+  icon = "__hidden-worlds__/graphics/icons/entity/waterMill.png"
 }
 waterMill.graphics_set = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"].graphics_set)
 
 ---@type data.ItemPrototype
 local waterMillItem = {
-    type = "item",
-    name = waterMill.name,
-    icon = waterMill.icon,
-    icon_size = 64,
-    flags = {},
-    subgroup = "extraction-machine",
-    --order = "za"..name,
-    place_result = waterMill.name,
-    stack_size = 50
+  type = "item",
+  name = waterMill.name,
+  icon = waterMill.icon,
+  icon_size = 64,
+  flags = {},
+  subgroup = "extraction-machine",
+  --order = "za"..name,
+  place_result = waterMill.name,
+  stack_size = 50
 }
 
 ---@type data.RecipePrototype
 local waterMillRecipe = {
-    name = "water-mill-recipe",
-    type = "recipe",
-    category = "basic-crafting",
-    ingredients = {
-    },
-    results = {
-        {type = "item", name = waterMillItem.name, amount = 1}
-    }
+  name = "water-mill-recipe",
+  type = "recipe",
+  category = "basic-crafting",
+  ingredients = {
+  },
+  results = {
+    {type = "item", name = waterMillItem.name, amount = 1}
+  }
 }
 
 ---@diagnostic disable-next-line: assign-type-mismatch
