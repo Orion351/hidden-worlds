14bdb6533a85b5323660f973e657b7de311e0e66
Jurgen <jurgen.baas@outlook.com>
2025-05-30 17:58:06 +0200

    Changed graphics to use grey boxes instead of electric furnace

--- prototypes/domains/woods/log-splitter.lua
+++ prototypes/domains/woods/log-splitter.lua
@@ -1,60 +1,79 @@
 ---@type data.FurnacePrototype
 local splitterEntity = {
-    name = "log-splitter",
-    type = "furnace",
-    crafting_speed = 1,
-    minable = {mining_time = 0.2, result = "log-splitter"},
-    energy_usage = "100kW",
-    energy_source = {
-        type = "fluid",
-        --fluid_usage_per_tick = 1,
-        burns_fluid = true,
-        scale_fluid_usage = true,
-        fluid_box = {
-            volume = 100,
-            filter = "torque",
-            production_type = "input",
-                pipe_connections = {{
-                    flow_direction = "input",
-                    direction = defines.direction.north --[[@as data.Direction]],
-                    position = {0, -1}
-            }}
+  name = "log-splitter",
+  type = "furnace",
+  crafting_speed = 1,
+  minable = {mining_time = 0.2, result = "log-splitter"},
+  energy_usage = "100kW",
+  energy_source = {
+    type = "fluid",
+    --fluid_usage_per_tick = 1,
+    burns_fluid = true,
+    scale_fluid_usage = true,
+    fluid_box = {
+      volume = 100,
+      filter = "torque",
+      production_type = "input",
+      pipe_connections = {{
+        flow_direction = "input",
+        direction = defines.direction.north --[[@as data.Direction]],
+        position = {0, -1}
+      }}
+    }
+  },
+  crafting_categories = {
+    "log-splitter"
+  },
+  collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+  selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  source_inventory_size = 1,
+  result_inventory_size = 1,
+  icon = "__hidden-worlds__/graphics/icons/entity/logSplitter.png",
+  graphics_set = {
+    animation = {
+      north = {
+        layers = {
+          {
+            filename = "__hidden-worlds__/graphics/greybox/grey-boxes/hw-grey-box-3x3.png",
+            width = 192,
+            height = 192,
+            scale = 0.5
+          },
+          {
+            filename = "__hidden-worlds__/graphics/greybox/primitive-titles/hw-label-log-splitter.png",
+            width = 128,
+            height = 64,
+            scale = 0.5
+          }
         }
-    },
-    crafting_categories = {
-        "log-splitter"
-    },
-    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    source_inventory_size = 1,
-    result_inventory_size = 1,
-    icon = "__hidden-worlds__/graphics/icons/entity/logSplitter.png"
+      }
+    }
+  }
 }
-splitterEntity.graphics_set = table.deepcopy(data.raw["furnace"]["electric-furnace"].graphics_set)
 
 ---@type data.ItemPrototype
 local splitterItem = {
-    type = "item",
-    name = splitterEntity.name,
-    icon = splitterEntity.icon,
-    icon_size = 64,
-    flags = {},
-    subgroup = "extraction-machine",
-    place_result = splitterEntity.name,
-    stack_size = 50
+  type = "item",
+  name = splitterEntity.name,
+  icon = splitterEntity.icon,
+  icon_size = 64,
+  flags = {},
+  subgroup = "extraction-machine",
+  place_result = splitterEntity.name,
+  stack_size = 50
 }
 
 ---@type data.RecipePrototype
 local splitterRecipe = {
-    name = "log-splitter-recipe",
-    type = "recipe",
-    category = "basic-crafting",
-    ingredients = {
-        -- TODO
-    },
-    results = {
-        {type = "item", name = splitterItem.name, amount = 1}
-    }
+  name = "log-splitter-recipe",
+  type = "recipe",
+  category = "basic-crafting",
+  ingredients = {
+    -- TODO
+  },
+  results = {
+    {type = "item", name = splitterItem.name, amount = 1}
+  }
 }
 
 ---@diagnostic disable-next-line: assign-type-mismatch
